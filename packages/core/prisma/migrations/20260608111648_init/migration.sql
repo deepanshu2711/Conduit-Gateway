b9c744a8fbad4c5e8b116411a7fc3d3f67be389a
Deepanshu <deepanshusaini2711@gmail.com>
2026-06-08 17:34:14 +0530

    feat: initialize Core

---
+++ packages/core/prisma/migrations/20260608111648_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Consumer" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Consumer_pkey" PRIMARY KEY ("id")
+);
---
+++ packages/core/src/server.ts
@@ -0,0 +1,5 @@
+import { app } from "./app.js";
+
+app.listen({ port: 3000 }, () => {
+  console.log("Gateway running on port 3000");
+});
